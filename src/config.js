98715c1f0b9e0814771189c35396291c7ed2e930
David <it.davidkaiser@gmail.com>
2023-07-03 14:33:17 -0700

    jeeze, youtube aint playing.

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 let API_URL = ''
 
 if (process.env.NODE_ENV === 'production') {
-  API_URL = 'http://161.35.236.75:3000'
+  API_URL = 'https://seven-donuts-add.loca.lt'
 } else {
   API_URL = 'http://localhost:3000'
 }
