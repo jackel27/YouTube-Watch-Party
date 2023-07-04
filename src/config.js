41b47e65e9256c696aa091eb9f4bf45d41c5144d
David <it.davidkaiser@gmail.com>
2023-07-03 17:39:12 -0700

    cross origin fix

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 let API_URL = ''
 
 if (process.env.NODE_ENV === 'production') {
-  API_URL = 'https://8aaa-161-35-236-75.ngrok-free.app'
+  API_URL = 'https://0ae4-161-35-236-75.ngrok-free.app'
 } else {
   API_URL = 'http://localhost:3000'
 }
